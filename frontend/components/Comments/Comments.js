2f2d1af84f373bbb4528f907775d45983bb09c47
Ajani Maragh <amsbstart@gmail.com>
2024-12-19 22:18:05 -0700

    finished module

--- frontend/components/Comments/Comments.js
+++ frontend/components/Comments/Comments.js
@@ -4,10 +4,11 @@ import Comment from './Comment';
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
   const { comments } = props;
-
+  
   return (
     <div className='comments-wrapper'>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(com => <Comment comment={com} key={com.id} />)}
     </div>
   );
 };
--- frontend/components/Posts/Posts.js
+++ frontend/components/Posts/Posts.js
@@ -4,10 +4,12 @@ import Post from './Post';
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
+  
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+    {posts.map(po => <Post post={po} likePost={likePost} key={po.id} />)}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
